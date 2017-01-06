9fefc59175b0fc26ae4445b89aae97ff8b6492fa
Aurélien <colombet.aurelien@etu.unice.fr>
2017-01-06 18:38:59 +0100

    Fix bug methode isSameHour

--- app/src/main/java/fr/unice/polytech/iam/utils/Macumba.java
+++ app/src/main/java/fr/unice/polytech/iam/utils/Macumba.java
@@ -86,10 +86,10 @@ public class Macumba {
     }
 
     private static boolean isSameHour(Calendar c1, Calendar c2) {
-        return c1.get(Calendar.YEAR) == c1.get(Calendar.YEAR)
-                || c1.get(Calendar.MONTH) == c2.get(Calendar.MONTH)
-                || c1.get(Calendar.DAY_OF_MONTH) == c2.get(Calendar.DAY_OF_MONTH)
-                || c1.get(Calendar.HOUR_OF_DAY) == c2.get(Calendar.HOUR_OF_DAY);
+        return c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR)
+                && c1.get(Calendar.MONTH) == c2.get(Calendar.MONTH)
+                && c1.get(Calendar.DAY_OF_MONTH) == c2.get(Calendar.DAY_OF_MONTH)
+                && c1.get(Calendar.HOUR_OF_DAY) == c2.get(Calendar.HOUR_OF_DAY);
     }
 
     private static boolean isWeekDay(int dayOfWeek) {
